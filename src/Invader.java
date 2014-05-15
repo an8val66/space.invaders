eef7c6f140479159d873e23ad51d60d0b91e8641
Brunomachadob <brunin5010@gmail.com>
2014-05-15 17:01:15 -0300

    Update Invader.java

--- src/Invader.java
+++ src/Invader.java
@@ -22,9 +22,11 @@ public class Invader
         icon = new ImageIcon( getClass().getResource( "/Sprites/invader.png" ) ).getImage();
         iw = icon.getWidth( null );
         ih = icon.getHeight( null );
+        
         // x e y calculados usando a area
         x = (int) ( iw / 2 + Math.random() * ( a.width - iw ) );
-        y = (int) ( ih / 2 + Math.random() * ( a.height - 100 - ih ) );
+        y = (int) ( ih / 2 + Math.random() * ( a.height - 250 - ih ) );
+        
         // dx e dy randomicos
         while ( dx == 0 || dy == 0 ) {
             
@@ -44,7 +46,7 @@ public class Invader
             if ( x < iw / 2 ) { dx = -dx; x += dx; }
             if ( y < ih / 2 ) { dy = -dy; y += dy; }
             if ( x > area.width - iw / 2 ) { dx = -dx; x += dx; }
-            if ( y > area.height - 100 - ih / 2 ) { dy = -dy; y += dy; }
+            if ( y > area.height -  ih / 2 ) { dy = -dy; y += dy; }
         }
         
     }
